695b091c906f50e5938c88ef1964c5ef61a50550
José Sánchez <joseynene@hotmail.com>
2017-04-04 23:05:44 +0200

    Check Connection with Firebase

--- Scoops/AppDelegate/AppDelegate.swift
+++ Scoops/AppDelegate/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // Se levanta el Singleton de Firebase. Se levanta el cliente de Firebase
+        FIRApp.configure()
+        
         return true
     }
 
