a9b453b949eb16ef290076db0c7dadac716cc172
José Sánchez <joseynene@hotmail.com>
2017-04-05 02:17:19 +0200

    Add Slider with steps and Label Rate

--- Scoops/ViewControllers/All users/PostReview.swift
+++ Scoops/ViewControllers/All users/PostReview.swift
@@ -10,9 +10,11 @@ import UIKit
 import Firebase
 
 class PostReview: UIViewController {
-
+    
     //MARK: - Outlets
     @IBOutlet weak var rateSlider: UISlider!
+    @IBOutlet weak var rateLabel: UILabel!
+    @IBOutlet weak var slideLabel: UILabel!
     @IBOutlet weak var imagePost: UIImageView!
     @IBOutlet weak var postTxt: UITextField!
     @IBOutlet weak var titleTxt: UITextField!
@@ -25,6 +27,13 @@ class PostReview: UIViewController {
         
         // Se indica analítica de Pantalla
         FIRAnalytics.setScreenName("PostReview", screenClass: "AllUsers")
+        
+        // Se establece el valor de las valoraciones
+        rateSlider.value = 3
+        slideLabel.text = String(Int(rateSlider.value))
+        //rateLabel.text = String(Int(rateSlider.value))
+        rateLabel.text = String(Int(4))
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -36,8 +45,12 @@ class PostReview: UIViewController {
     @IBAction func rateAction(_ sender: Any) {
         // Se indica analítica de acción
         FIRAnalytics.logEvent(withName: "rateAction", parameters: ["posts" : "reviewposts" as NSObject])
-        
-        print("\((sender as! UISlider).value)")
+
+        // Se establecen los pasos del slider para que solo se puedan seleccionar valores enteros
+        rateSlider.value = roundf(rateSlider.value)
+        // Se asigna al indicador de estrellas
+        slideLabel.text = String(Int(rateSlider.value))
+        print("slider: \(rateSlider.value)")
     }
 
     
